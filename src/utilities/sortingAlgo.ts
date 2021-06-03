b3db1e035f353a63fd33e7f4523d595c8f521e6e
Akarsh Saraff <saraffakarsh@gmail.com>
2021-06-03 18:58:35 +0100

    sorting alogithm changes

--- src/utilities/sortingAlgo.ts
+++ src/utilities/sortingAlgo.ts
@@ -8,27 +8,22 @@
  *  if the filter is Asset type, it will return the sorted array by the order of ['Equities', 'Macro', 'Credit'].
  */
 
-export const sorter = (filter, list) => {
-
+export const sorter = (filter:string, list: any) => {
     if(filter === 'Ticker') {
         list.sort((item1, item2) => {
             return item1.ticker.localeCompare(item2.ticker);
         })
     }
-
     if(filter === 'Price') {
         list.sort((item1, item2) => {
-            // return item1.price - item2.price; // ascending order
             return item2.price - item1.price;
          })
     }
-
     if(filter === 'Asset Type') {
         const filter = ["Equities", "Macro", "Credit"];
         list.sort((item1, item2) => {
             return filter.indexOf(item1.assetClass) - filter.indexOf(item2.assetClass);
         })
     }
-
     return list;
 }
