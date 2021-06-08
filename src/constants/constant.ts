fdc74c7808bd06677a21f07f712dd45b557ff605
Akarsh Saraff <saraffakarsh@gmail.com>
2021-06-08 17:37:12 +0100

    removed the commented code

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,6 +1,4 @@
 
-//export const listApiUrl:string = 'http://localhost:3333/list';
-
 export const listApiUrl: string = `${window.location.protocol}//${window.location.hostname}:3333/list`;
 export const tableHeaderTitles: string[] = ['Ticker', 'Price', 'Asset Type'];
 export const loadingText:string = 'Loading ......'
